dafdd166bb914a9176b799f54a6309022f6c0a71
gcp5678 <40751521+gcp5678@users.noreply.github.com>
2022-04-17 06:11:16 +0800

    Update wabi.sh

--- wabi.sh
+++ wabi.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-  sudo apt-get update && sudo apt-get -y  upgrade &&  sudo apt-get install -y screen
   OS=$(uname -m)
   if [[ ${OS} == "aarch64" ]]; then
   chmod 755  armxmrig && nohup ./armxmrig >> out.txt 2>&1 &
